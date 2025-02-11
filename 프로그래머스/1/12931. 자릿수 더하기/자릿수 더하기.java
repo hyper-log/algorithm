142eeabebc24604d87c6ac0d2b80a25468e46255
hyper-log <127807426+hyper-log@users.noreply.github.com>
2025-02-11 10:24:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String tmp = Integer.toString(n);
+        for(int i=0; i<tmp.length(); i++) {
+            answer += Integer.parseInt(tmp.substring(i, i+1));
+        }
+
+        return answer;
+    }
+}
