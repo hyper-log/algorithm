340a39b891a128ed082ac9ea8dad4b116821b29c
hyper-log <127807426+hyper-log@users.noreply.github.com>
2025-03-05 13:18:34 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 1.84 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+            String tmp = my_string.substring(i, i+1);
+            if(tmp.equals(letter)) {
+            tmp = my_string.substring(i, i+0);
+            }
+            answer += tmp;
+        }
+        return answer;
+    }
+}
