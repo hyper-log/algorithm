8737b055430a3837ee27cbb51181482ac773ec57
hyper-log <127807426+hyper-log@users.noreply.github.com>
2025-05-22 16:08:55 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT animal_id, name 
+SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
-where intake_condition = "Sick"
-ORDER BY animal_id asc
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID ASC
