b527383748240ae8b42ddf78022c56efa5142526
hyper-log <127807426+hyper-log@users.noreply.github.com>
2025-05-22 16:40:07 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
-SELECT animal_id, name, date_format(datetime, '%Y-%m-%d') as 날짜
-FROM animal_ins
-order by animal_id asc
+/** 2회차 **/
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, '%Y-%m-%d') as 날짜
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC
