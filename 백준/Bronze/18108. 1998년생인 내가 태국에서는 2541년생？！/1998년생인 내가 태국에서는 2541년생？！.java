d01719d0a0af97d08e2c804f4be0067c3581723e
hyper-log <127807426+hyper-log@users.noreply.github.com>
2025-08-06 10:21:01 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 92 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int byear=sc.nextInt();
+        
+        System.out.println(byear-543);
+        
+    }
+}
